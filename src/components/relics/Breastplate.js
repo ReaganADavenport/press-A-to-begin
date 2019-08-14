c3dd69c0f1b618d8d090418b0e3cfd18d448101b
Reagan Davenport <reagandavenport@Reagans-MacBook-Air.local>
2019-08-13 22:31:23 -0400

    made documentation for everything on the lance, breastplate, and shoes pages

--- src/components/relics/Breastplate.js
+++ src/components/relics/Breastplate.js
@@ -9,21 +9,27 @@ const Breastplate = () => {
         <div className="Relic">
         <h1>The Breastplate of Saint Patrick:</h1>
             <ul>
-                <li>a Prayer of Protection, traditionally attributed to Saint Patrick during his Irish ministry in the 5th century.</li>
+                <li>a Prayer of Protection, traditionally attributed to Saint Patrick during his Irish ministry in the 5th century. <sup>1</sup> </li>
                 <li>Christ, be with me, Christ before me, Christ behind me,</li>
                 <li>Christ in me, Christ beneath me, Christ above me,</li>
                 <li>Christ on my right, Christ on my left, Christ where I lie, Christ where I sit,</li>
                 <li>Christ where I arise, Christ in the heart of every man who thinks of me,</li>
                 <li>Christ in the mouth of every man who speaks of me,</li>
-                <li>Christ in every eye that sees me, Christ in every ear that hears me</li>
+                <li>Christ in every eye that sees me, Christ in every ear that hears me <sup>2</sup></li>
             </ul>
 
             <h3>More on St. Patrick</h3>
             <img src={StPatty} alt='StPatty'/>
             <ul>
-                <li>Saint Patrick was born around 390 (likely in 387), at Kilpatrick, near Dumbarton, in Scotland. His name is from the Latin Patricius, meaning high-born. His parents were part of the Christian minority of Britain; his father, Calpurnius, was a deacon, "the son of Potitus, a priest, of the village Bannavem Taburniæ." </li>
-                <li>At the age of 16, he was captured during a raiding party and taken to Ireland as a slave to herd and tend sheep. During that time, he prayed frequently and came for the first time to have a true faith in God. At age 22, he had a vision in which God told him to be prepared to leave Ireland. Soon, he escaped, walking 200 miles to a ship and returning to England. In a dream, he saw the people of Ireland calling him, "We beg you, holy youth, that you shall come and shall walk again among us." </li>
-                <li>St Patrick sought clerical training. He was ordained by St. Germanus, bishop of Auxerre. Around 430 he was ordained a bishop, after which he returned to Ireland. There, he preached the Gospel, reaching tribal chieftains, gaining their permission to teach their subjects also. During his episcopate, he was attacked for a sin he confessed to a close friend, a sin he committed "in a single hour" when only 15, but he did not suffer as a result. He established an episcopal administration and led a monastic lifestyle, establishing Christianity in Ireland. St. Patrick died at Saul, Downpatrick, Ireland, on March 17, 461.</li>
+                <li>Saint Patrick was born around 390 (likely in 387), at Kilpatrick, near Dumbarton, in Scotland. His name is from the Latin Patricius, meaning high-born. His parents were part of the Christian minority of Britain; his father, Calpurnius, was a deacon, "the son of Potitus, a priest, of the village Bannavem Taburniæ." <sup>3</sup> </li>
+                <li>At the age of 16, he was captured during a raiding party and taken to Ireland as a slave to herd and tend sheep. During that time, he prayed frequently and came for the first time to have a true faith in God. At age 22, he had a vision in which God told him to be prepared to leave Ireland. Soon, he escaped, walking 200 miles to a ship and returning to England. In a dream, he saw the people of Ireland calling him, "We beg you, holy youth, that you shall come and shall walk again among us." <sup>3</sup> </li>
+                <li>St Patrick sought clerical training. He was ordained by St. Germanus, bishop of Auxerre. Around 430 he was ordained a bishop, after which he returned to Ireland. There, he preached the Gospel, reaching tribal chieftains, gaining their permission to teach their subjects also. During his episcopate, he was attacked for a sin he confessed to a close friend, a sin he committed "in a single hour" when only 15, but he did not suffer as a result. He established an episcopal administration and led a monastic lifestyle, establishing Christianity in Ireland. St. Patrick died at Saul, Downpatrick, Ireland, on March 17, 461. <sup>3</sup> </li>
+            </ul>
+
+            <ul>
+                <li> <sup>1</sup> <a href="https://en.wikipedia.org/wiki/Saint_Patrick%27s_Breastplate">https://en.wikipedia.org/wiki/Saint_Patrick%27s_Breastplate</a> </li>
+                <li> <sup>2</sup> <a href="https://www.catholic.org/prayers/prayer.php?p=3290">https://www.catholic.org/prayers/prayer.php?p=3290</a> </li>
+                <li> <sup>3</sup> <a href="https://orthodoxwiki.org/Patrick_of_Ireland">https://orthodoxwiki.org/Patrick_of_Ireland</a> </li>
             </ul>
         </div>
     )
--- src/components/relics/Shoes.js
+++ src/components/relics/Shoes.js
@@ -9,23 +9,27 @@ const Shoes = () => {
         <div className="Relic">
         <h1>The Shoes of St. Spyridon:</h1>
         <ul>
-            <li>Saint Spyridon's silk slippers which clothe the feet of his relics wear out each year and are replaced on his feast day.</li>
+            <li>Saint Spyridon's silk slippers which clothe the feet of his relics wear out each year and are replaced on his feast day.<a href="https://orthodoxwiki.org/Spyridon_of_Trimythous"><sup>1</sup></a></li>
         </ul>
             <h3>The life of St. Spyridon</h3>
             <img src={StSpyri} alt="StSpyri"/>
         <ul>
-            <li>Our father among the saints Spyridon of Trimythous the Wonderworker ca. 270-348) was a fourth century bishop who was present at the First Ecumenical Council. He is also commonly referred to in Corfu as Keeper of the City  since he is also the patron saint of that island (this is where his relics are located and venerated). He is commemorated by the church on December 12. </li>
-            <li>Spyridon was born in the village of Ashia Cyprus (270 AD) and died in Trimythous, Cyprus (348 AD). He was a peasant farmer and shepherd and was not educated. Spyridon was married and had a daughter, Irene. After his wife died, he and his daughter both entered into monasticism. He later became the Bishop of Trimythous (during the reign of Constantine the Great) and continued in piety for which he was greatly known.</li>
-            <li>He is the patron saint of potters from the miracle where pulled the fire, water, and dust that made up a pot shard to prove the Holy Trinity. </li>
+            <li>Our father among the saints Spyridon of Trimythous the Wonderworker ca. 270-348) was a fourth century bishop who was present at the First Ecumenical Council. He is also commonly referred to in Corfu as Keeper of the City  since he is also the patron saint of that island (this is where his relics are located and venerated). He is commemorated by the church on December 12. <sup>1</sup> </li>
+            <li>Spyridon was born in the village of Ashia Cyprus (270 AD) and died in Trimythous, Cyprus (348 AD). He was a peasant farmer and shepherd and was not educated. Spyridon was married and had a daughter, Irene. After his wife died, he and his daughter both entered into monasticism. He later became the Bishop of Trimythous (during the reign of Constantine the Great) and continued in piety for which he was greatly known. <sup>1</sup> </li>
+            <li>He is the patron saint of potters from the miracle where pulled the fire, water, and dust that made up a pot shard to prove the Holy Trinity.<sup>1</sup> </li>
         </ul>
             <h3>Persecutions and witness to Orthodox Faith</h3>
         <ul>
-            <li>During the Maximilian persecutions (295 AD) he was arrested and exiled. In 325 AD, he was present at the First Ecumenical Council of Nicea where he astonished many with his simple explanations of the Orthodox Faith. Though not well-educated, he surprised everyone at the Council of Nicea by converting a famous philosopher to Orthodoxy from Arianism. He explained the unity and diversity of the Trinity by holding up a brick, then commonly thought to be a combination of the elements fire, earth and water; as he spoke, fire blazed from the top of the brick while water gushed out underneath. There too, at Nicea, he met St. Nicholas of Myra, with whom he formed a lasting friendship.</li>
+            <li>During the Maximilian persecutions (295 AD) he was arrested and exiled. In 325 AD, he was present at the First Ecumenical Council of Nicea where he astonished many with his simple explanations of the Orthodox Faith. Though not well-educated, he surprised everyone at the Council of Nicea by converting a famous philosopher to Orthodoxy from Arianism. He explained the unity and diversity of the Trinity by holding up a brick, then commonly thought to be a combination of the elements fire, earth and water; as he spoke, fire blazed from the top of the brick while water gushed out underneath. There too, at Nicea, he met St. Nicholas of Myra, with whom he formed a lasting friendship. <sup>1</sup> </li>
         </ul>
             <h3>Departure and relics</h3>
         <ul>
-            <li>St. Spyridon lived most of his earthly life in Cyprus, where he also reposed in 348 AD and was buried in the Church of the Holy Apostles in Trimythous. </li>
-            <li>When the Saracens took the island, the Cypriots opened his grave in order to remove his sacred bones to Constantinople. They found that his body had remained intact, while from the grave emanated a scent of basil, true signs of the sainthood he had shown during his lifetime. When Constantinople fell in 1453, he was transferred to Serbia, then a Corfiot elder, Georgios Kalohairetis, brought him to the island of Corfu in Greece where he is currently buried. </li>
+            <li>St. Spyridon lived most of his earthly life in Cyprus, where he also reposed in 348 AD and was buried in the Church of the Holy Apostles in Trimythous. <sup>1</sup> </li>
+            <li>When the Saracens took the island, the Cypriots opened his grave in order to remove his sacred bones to Constantinople. They found that his body had remained intact, while from the grave emanated a scent of basil, true signs of the sainthood he had shown during his lifetime. When Constantinople fell in 1453, he was transferred to Serbia, then a Corfiot elder, Georgios Kalohairetis, brought him to the island of Corfu in Greece where he is currently buried. <sup>1</sup> </li>
+        </ul>
+
+        <ul>
+            <li> <sup>1</sup> <a href="https://orthodoxwiki.org/Spyridon_of_Trimythous">https://orthodoxwiki.org/Spyridon_of_Trimythous</a></li>
         </ul>
         </div>
     )
