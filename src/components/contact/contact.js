a2be6bad0f4dafc2db24951b1b16862687ee77fe
Reagan Davenport <reagandavenport@Reagans-MacBook-Air.local>
2019-08-21 14:04:05 -0400

    added photo to the contact page

--- src/components/contact/contact.js
+++ src/components/contact/contact.js
@@ -1,6 +1,6 @@
 import React from 'react';
-// import styled from 'styled-components';
-// import { Link } from 'react-router-dom';
+
+import Scott from '../../images/Scott_Davenport.JPG';
 
 import './contact.css';
 
@@ -8,6 +8,8 @@ const Home = () => {
     return(
         <div className="Contact">
         <h1>Contact Information:</h1>
+        <img src={Scott} alt="Scott"/>
+        
         <p>Email: scott@pressatobegin.com</p>
         <p>FaceBook: <a href="https://m.facebook.com/Press-A-To-Begin-104593057555460/?tsid=0.23366555156080127&source=result">facebook.com/Press-A-To-Begin</a></p>
         <p>Instagram: <a href="https://www.instagram.com/Pressatobegin_/">instagram.com/Pressatobegin_</a></p>
